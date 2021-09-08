4b52460e1fed18772f6271703c03432c5ad18ac5
zbarnz <zbarnz@yahoo.com>
2021-09-08 15:18:21 -0600

    commit

--- post.sh
+++ post.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+FILEONE=./one.txt
+FILETWO=./two.txt
+
+if test -f "$FILEONE"; then
+  mv one.txt two.txt
+fi
+
+if test -f "$FILETWO"; then
+  mv two.txt one.txt
+fi
+
 git add . 
 
 git commit -m "commit" 
