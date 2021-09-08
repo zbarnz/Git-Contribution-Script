9ba0e0d75649f758fb027af58952cd7147eb590e
zbarnz <zbarnz@yahoo.com>
2021-09-08 15:27:24 -0600

    commit

--- post.sh
+++ post.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-FILEONE=./one.txt
 FILETWO=./two.txt
 
 if test -f "$FILETWO"; then
   mv two.txt one.txt
-  echo "two"
-fi
-
-if [ -f "$FILEONE" ]; then
+else
   mv one.txt two.txt
-  echo "1"
 fi
 
 git add . 
